cbaf8c7509c93c6b7d6f2500b70d211a1f4641ce
Mike Bennett <mike.bennett@ed.ac.uk>
2017-06-15 13:03:10 +0100

    process the results to eliminate one-item lists

--- lunadata_process.py
+++ lunadata_process.py
@@ -31,6 +31,31 @@ results = solr_query(s, query, fields=fields, sort=sort)
 # (I know this is very bad program design, but I intend to properly module-ise moonsun_miner in the near future,
 #  to a class-based design, which will make this issue irrelevant!)
 
-# Show the first few results
+
+# Lets loop through the results, and spit them out to a CSV
+# Unfortunately, because the fields are 'multivalued' in Solr, the actual data always comes as a list even though
+# all the actual data fields only have one entry.
+# We can fix that in two ways:
+# a) Loop through the results, and reduce the one-item lists to strings
+# b) Use the equivalent _sortable fields in Solr for the query
+#
+# I am tending towards solution a here, because the _sortable fields may well have been modified by the Solr routines
+# that prepare text to be searchable (i.e you might get "Bronte" where the original data says "Brontë")
+
+# Here is a routine implementing solution a
+def reduce_singles(source_list):
+    '''Take a list of dictionaries and collapse single item lists within the dictionary values'''
+    new_list = []
+    for result in source_list:
+        # This should be a dictionary of a single result, so let's loop it and get rid of single item lists
+        for item, val in result.items():
+            if isinstance(val, list) and len(val) == 1:
+                val = val[0]
+                result[item] = val
+        new_list.append(result)
+    return new_list
+
+# Process the results as above and show a couple to check it's working
 from pprint import pprint
-pprint(results[0:10])
+new_results = reduce_singles(results)
+pprint(new_results[0:10])
